31028186faa7175c5898d0a40769756b3f0f98a9
tubone <tubo.yyyuuu@gmail.com>
2025-03-31 13:05:19 +0900

    🔧 fix(workflows): 環境変数を更新してGitHub Actionsの設定を改善する 🔧 fix(config): Astroサイトの設定をVITE環境変数に合わせて修正する

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import path from 'path';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // 環境変数
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const isGitHubPages = process.env.VITE_GITHUB_ACTIONS === 'true';
+const repositoryName = process.env.VITE_GITHUB_REPOSITORY?.split('/')[1] || '';
 
 export default defineConfig({
     site: isGitHubPages
-        ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${repositoryName}/`
+        ? `https://${process.env.VITE_GITHUB_REPOSITORY_OWNER}.github.io/${repositoryName}/`
         : 'http://localhost:3000',
     base: isGitHubPages ? `/${repositoryName}/` : '/',
     output: 'static',
